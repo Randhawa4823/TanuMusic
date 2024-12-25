e76a727502d79a99f388e31315fad796343a09e0
Randhawa <166139767+Randhawa4823@users.noreply.github.com>
2024-12-25 19:43:00 +0530

    Update start.py

--- TanuMusic/plugins/bot/start.py
+++ TanuMusic/plugins/bot/start.py
@@ -88,7 +88,7 @@ async def start_pm(client, message: Message):
                          f"<b>● Username ➥</b> @{message.from_user.username}",
                 )
     else:
-        out = private_panel()
+        out = private_panel()  # Removed language argument
         try:
             await message.reply_photo(
                 random.choice(Photos),
@@ -111,7 +111,7 @@ async def start_pm(client, message: Message):
 
 @app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
 async def start_gp(client, message: Message):
-    out = start_panel()
+    out = start_panel()  # No language argument needed
     uptime = int(time.time() - _boot_)
     try:
         await message.reply_photo(
@@ -145,7 +145,7 @@ async def welcome(client, message: Message):
                 )
                 return await app.leave_chat(message.chat.id)
 
-            out = start_panel()
+            out = start_panel()  # No language argument needed
             try:
                 await message.reply_photo(
                     random.choice(Photos),
